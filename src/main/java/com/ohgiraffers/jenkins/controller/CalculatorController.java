93848df1a81993222a327ca9d6ad9ad8b8146580
jungho <kiik52@naver.com>
2025-07-03 10:47:19 +0900

    modify : CalculatorController file-2

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "ArgoCD setting > 🦊🦊🦊🦊";
+        return "ArgoCD setting > 🦊🦊";
     }
 
     @GetMapping("/plus")
